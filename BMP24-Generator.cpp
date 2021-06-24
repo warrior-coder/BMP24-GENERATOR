48a56772d18c97d3c3341465940ec73b842c9cbf
warrior-coder <gudkou2003@gmail.com>
2021-06-24 16:19:25 +0300

    All inside IMAGE class

--- BMP24-Generator.cpp
+++ BMP24-Generator.cpp
@@ -1,8 +1,8 @@
 ﻿#include <cmath>
 #include <stdio.h>
-#define PI 3.14159265358979323846
 
 typedef unsigned char BYTE;
+
 typedef struct
 {
     BYTE B;
@@ -10,197 +10,223 @@ typedef struct
     BYTE R;
 } RGB;
 
-// Buffer - part of file which contains pixels data
-RGB** buffer;
-short bw, bh;
-
-void set_pixel(short x, short y)
+class IMAGE
 {
-    if (x > -1 && y > -1 && x < bw && y < bh)
+public:
+    RGB** buffer = nullptr;
+    short width;
+    short height;
+    RGB pen_color = {};
+
+    IMAGE(short imageWidth, short imageHeight)
     {
-        buffer[y][x] = { 255,255,255 };
-    }
-}
+        width = imageWidth;
+        height = imageHeight;
 
-void fill_circle(short x0, short y0, float R)
-{
-    float f;
-
-    for (float x = x0 - R; x < x0 + R; x++)
-    {
-        for (float y = y0 - R; y < y0 + R; y++)
+        buffer = new RGB* [height];
+        for (short y = 0; y < height; y++)
         {
-            f = (x - x0) * (x - x0) + (y - y0) * (y - y0) - R*R;
-            if (f < 0) set_pixel((short)x, (short)y);
-        }
-    }
-}
+            buffer[y] = new RGB[width];
 
-void write_bmp(const char* fname, short img_width, short img_height)
-{
-    // -+-+-+-+-+-+-+-+-+-+- GENERATE BUFFER -+-+-+-+-+-+-+-+-+-+-
-    // Create buffer
-    bw = img_width;
-    bh = img_height;
-    buffer = new RGB*[bh];
-
-    // Draw gradient background
-    for (short y = 0; y < bh; y++)
-    {   
-        buffer[y] = new RGB[bw];
-        for (short x = 0; x < bw; x++)
-        {
-            buffer[y][x].R = (BYTE)(255 - (float)x / bw * 255);
-            buffer[y][x].G = (BYTE)((float)x / bw * 255);
-            buffer[y][x].B = (BYTE)((float)y / bh * 255);
+            for (short x = 0; x < width; x++)
+            {
+                buffer[y][x] = {};
+            }
         }
     }
 
-    // Draw C++
-    short x, y;
-    for (float deg = -90; deg < 90; deg += 0.5)
+    ~IMAGE()
     {
-        x = (short)round(img_width / 3 - cos(deg / 180 * PI) * img_height / 4);
-        y = (short)round(img_height / 2 + sin(deg / 180 * PI) * img_height / 4);
-        fill_circle(x, y, 8);
-    }
-    for (short i = -bh/6; i < bh/6; i++)
-    {
-        fill_circle(bw/6*2 + i, bh/2, 8);
-        fill_circle(bw/6*2, bh/2 + i, 8);
-        fill_circle(bw/12*9 + i, bh/2, 8);
-        fill_circle(bw/12*9, bh/2 + i, 8);
-    }
-
-    // -+-+-+-+-+-+-+-+-+-+- GENERATE BOF -+-+-+-+-+-+-+-+-+-+-
-    // File header
-    const short f_header_size = 14;
-    BYTE f_header[f_header_size];
-
-    // File info
-    const short f_info_size = 40;
-    BYTE f_info[f_info_size];
-
-    // File size
-    int p_size = (4 - (img_width * 3) % 4) % 4;
-    int fsize = f_header_size + f_info_size + (img_width * img_height * 4) + (p_size * img_height);
-
-    // -+-+-+-+-+-+-+-+-+-+- HEADER - 14 BYTES -+-+-+-+-+-+-+-+-+-+-
-    // File type
-    f_header[0] = 'B';
-    f_header[1] = 'M';
-
-    // File size, Max file size = 2^(4 * 8) = 4.294.967.296 bits = 536.870.912 bytes
-    f_header[2] = fsize;
-    f_header[3] = fsize >> 8;
-    f_header[4] = fsize >> 16;
-    f_header[5] = fsize >> 24;
-
-    // Reserved (Not used)
-    f_header[6] = 0;
-    f_header[7] = 0;
-
-    // Reserved (Not used)
-    f_header[8] = 0;
-    f_header[9] = 0;
-
-    // Pixel data offset
-    f_header[10] = f_header_size + f_info_size;
-    f_header[11] = 0;
-    f_header[12] = 0;
-    f_header[13] = 0;
-
-    // -+-+-+-+-+-+-+-+-+-+- INFO - 40 BYTES -+-+-+-+-+-+-+-+-+-+-
-    // Header size
-    f_info[0] = f_info_size;
-    f_info[1] = 0;
-    f_info[2] = 0;
-    f_info[3] = 0;
-
-    // Image width
-    f_info[4] = img_width;
-    f_info[5] = img_width >> 8;
-    f_info[6] = img_width >> 16;
-    f_info[7] = img_width >> 24;
-
-    // Image height
-    f_info[8] = img_height;
-    f_info[9] = img_height >> 8;
-    f_info[10] = img_height >> 16;
-    f_info[11] = img_height >> 24;
-
-    // Planes
-    f_info[12] = 1;
-    f_info[13] = 0;
-
-    // Bits per pixel (RGB - 3*8 bits)
-    f_info[14] = 24;
-    f_info[15] = 0;
-
-    // Compression (No compression)
-    f_info[16] = 0;
-    f_info[17] = 0;
-    f_info[18] = 0;
-    f_info[19] = 0;
-
-    // Image size (No compression)
-    f_info[20] = 0;
-    f_info[21] = 0;
-    f_info[22] = 0;
-    f_info[23] = 0;
-
-    // X pixels per meter (Not specified)
-    f_info[24] = 0;
-    f_info[25] = 0;
-    f_info[26] = 0;
-    f_info[27] = 0;
-
-    // Y pixels per meter (Not specified)
-    f_info[28] = 0;
-    f_info[29] = 0;
-    f_info[30] = 0;
-    f_info[31] = 0;
-
-    // Total colors (Color palette not used)
-    f_info[32] = 0;
-    f_info[33] = 0;
-    f_info[34] = 0;
-    f_info[35] = 0;
-
-    // Important colors (Ignored)
-    f_info[36] = 0;
-    f_info[37] = 0;
-    f_info[38] = 0;
-    f_info[39] = 0;
-
-    // -+-+-+-+-+-+-+-+-+-+- WRITE FILE -+-+-+-+-+-+-+-+-+-+-
-    FILE* fp;
-
-    fopen_s(&fp, fname, "wb");
-    if (fp)
-    {
-        fwrite(f_header, sizeof(BYTE), f_header_size, fp);
-        fwrite(f_info, sizeof(BYTE), f_info_size, fp);
-
-        BYTE padding = 0;
-
-        for (short y = 0; y < img_height; y++)
+        if (buffer)
         {
-            fwrite(buffer[y], sizeof(RGB), img_width, fp);
-            fwrite(&padding, sizeof(BYTE), p_size, fp);
+            for (short i = 0; i < height; i++)
+            {
+                delete[] buffer[i];
+            }
+            delete[] buffer;
+            buffer = nullptr;
         }
-
-        fclose(fp);
-        delete[] buffer;
-
-        printf("File creadted.");
     }
-    else printf("File create error.");
-}
+
+    void set_pixel(short x, short y)
+    {
+        if (x > -1 && y > -1 && x < width && y < height)
+        {
+            buffer[y][x] = pen_color;
+        }
+    }
+
+    void set_circle(short x0, short y0, short R)
+    {
+        for (short x = x0 - R; x < x0 + R; x++)
+        {
+            for (short y = y0 - R; y < y0 + R; y++)
+            {
+                if (((x-x0)*(x-x0) + (y-y0)*(y-y0) - R*R) < 0) set_pixel(x, y);
+            }
+        }
+    }
+
+    void save(const char* fname)
+    {
+        const short f_header_size = 14;
+        BYTE f_header[f_header_size];
+
+        const short f_info_size = 40;
+        BYTE f_info[f_info_size];
+
+        int p_size = (4 - (width * 3) % 4) % 4;
+
+        int fsize = f_header_size + f_info_size + (width * height * 4) + (p_size * height);
+
+        /* HEADER - 14 BYTES */
+        // File signature
+        f_header[0] = 'B';
+        f_header[1] = 'M';
+
+        // File size, Max file size = 2^(4*8) = 4.294.967.296 bits = 536.870.912 bytes
+        f_header[2] = fsize;
+        f_header[3] = fsize >> 8;
+        f_header[4] = fsize >> 16;
+        f_header[5] = fsize >> 24;
+
+        // Reserved
+        f_header[6] = 0;
+        f_header[7] = 0;
+        f_header[8] = 0;
+        f_header[9] = 0;
+
+        // Pixel data offset
+        f_header[10] = f_header_size + f_info_size;
+        f_header[11] = 0;
+        f_header[12] = 0;
+        f_header[13] = 0;
+
+        /* INFO - 40 BYTES */
+        // Header size
+        f_info[0] = f_info_size;
+        f_info[1] = 0;
+        f_info[2] = 0;
+        f_info[3] = 0;
+
+        // Image width
+        f_info[4] = width;
+        f_info[5] = width >> 8;
+        f_info[6] = width >> 16;
+        f_info[7] = width >> 24;
+
+        // Image height
+        f_info[8] = height;
+        f_info[9] = height >> 8;
+        f_info[10] = height >> 16;
+        f_info[11] = height >> 24;
+
+        // Planes
+        f_info[12] = 1;
+        f_info[13] = 0;
+
+        // Bits per pixel (RGB - 3*8 = 24 bits)
+        f_info[14] = 24;
+        f_info[15] = 0;
+
+        // Compression (No compression)
+        f_info[16] = 0;
+        f_info[17] = 0;
+        f_info[18] = 0;
+        f_info[19] = 0;
+
+        // Image size (No compression)
+        f_info[20] = 0;
+        f_info[21] = 0;
+        f_info[22] = 0;
+        f_info[23] = 0;
+
+        // X pixels per meter
+        f_info[24] = 0;
+        f_info[25] = 0;
+        f_info[26] = 0;
+        f_info[27] = 0;
+
+        // Y pixels per meter
+        f_info[28] = 0;
+        f_info[29] = 0;
+        f_info[30] = 0;
+        f_info[31] = 0;
+
+        // Total colors (Color palette not used)
+        f_info[32] = 0;
+        f_info[33] = 0;
+        f_info[34] = 0;
+        f_info[35] = 0;
+
+        // Important colors (0 - all)
+        f_info[36] = 0;
+        f_info[37] = 0;
+        f_info[38] = 0;
+        f_info[39] = 0;
+
+        // Write file
+        FILE* fp;
+
+        fopen_s(&fp, fname, "wb");
+        if (fp)
+        {
+            // Write header
+            fwrite(f_header, sizeof(BYTE), f_header_size, fp);
+
+            // Write info
+            fwrite(f_info, sizeof(BYTE), f_info_size, fp);
+
+            // Write buffer
+            BYTE padding = 0;
+
+            for (short y = 0; y < height; y++)
+            {
+                fwrite(buffer[y], sizeof(RGB), width, fp);
+                fwrite(&padding, sizeof(BYTE), p_size, fp);
+            }
+
+            fclose(fp);
+
+            printf("File creadted.");
+        }
+        else printf("File creadte error.");
+    }
+};
 
 int main()
 {
-    write_bmp("result.bmp", 640, 640);
+    IMAGE img(640, 640);
+
+    // Set pen color
+    img.pen_color = { 255,255,255 };
+
+    // Draw gradient background
+    for (short y = 0; y < img.height; y++)
+    {
+        for (short x = 0; x < img.width; x++)
+        {
+            img.buffer[y][x].R = (BYTE)(255 - (float)x  / img.width * 255);
+            img.buffer[y][x].G = (BYTE)((float)x / img.width * 255);
+            img.buffer[y][x].B = (BYTE)((float)y / img.height * 255);
+        }
+    }
+    // Draw C++
+    for (float deg = 1.39626; deg < 4.88692; deg += .0174533)
+    {
+        img.set_circle((short)(230 + cos(deg) * 180), (short)(320 + sin(deg) * 180), 8);
+    }
+
+    for (short i = 0; i < 200; i++)
+    {
+        img.set_circle(130+i, 320, 8);
+        img.set_circle(230, 220+i, 8);
+        img.set_circle(380 + i, 320, 8);
+        img.set_circle(480, 220+i, 8);
+    }
+
+    // Save result
+    img.save("out.bmp");
 
     return 0;
 }
--- README.md
+++ README.md
@@ -11,31 +11,26 @@ write_bmp("result.bmp", 640, 640);
 ## Example of draw script: ##
 ```CPP
 // Draw gradient background
-for (short y = 0; y < bh; y++)
-{   
-    buffer[y] = new RGB[bw];
-    for (short x = 0; x < bw; x++)
+for (short y = 0; y < img.height; y++)
+{
+    for (short x = 0; x < img.width; x++)
     {
-        buffer[y][x].R = (BYTE)(255 - (float)x / bw * 255);
-        buffer[y][x].G = (BYTE)((float)x / bw * 255);
-        buffer[y][x].B = (BYTE)((float)y / bh * 255);
+        img.buffer[y][x].R = (BYTE)(255 - (float)x  / img.width * 255);
+        img.buffer[y][x].G = (BYTE)((float)x / img.width * 255);
+        img.buffer[y][x].B = (BYTE)((float)y / img.height * 255);
     }
 }
-
 // Draw C++
-short x, y;
-for (float deg = -90; deg < 90; deg += 0.5)
+for (float deg = 1.39626; deg < 4.88692; deg += .0174533)
 {
-    x = (short)round(img_width / 3 - cos(deg / 180 * PI) * img_height / 4);
-    y = (short)round(img_height / 2 + sin(deg / 180 * PI) * img_height / 4);
-    fill_circle(x, y, 8);
+    img.set_circle((short)(230 + cos(deg) * 180), (short)(320 + sin(deg) * 180), 8);
 }
-for (short i = -bh/6; i < bh/6; i++)
+for (short i = 0; i < 200; i++)
 {
-    fill_circle(bw/6*2 + i, bh/2, 8);
-    fill_circle(bw/6*2, bh/2 + i, 8);
-    fill_circle(bw/12*9 + i, bh/2, 8);
-    fill_circle(bw/12*9, bh/2 + i, 8);
+    img.set_circle(130+i, 320, 8);
+    img.set_circle(230, 220+i, 8);
+    img.set_circle(380 + i, 320, 8);
+    img.set_circle(480, 220+i, 8);
 }
 ```
 
